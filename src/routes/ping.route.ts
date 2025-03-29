77fded067008370b64fe4f58869c3623e27a2e52
ayrton cravero <ayrtoncravero26@gmail.com>
2025-03-29 10:27:43 -0300

    add documentation for ping module

--- src/routes/ping.route.ts
+++ src/routes/ping.route.ts
@@ -3,6 +3,23 @@ import { PingController } from '../controllers/ping.controller';
 const router = Router();
 const pingController = new PingController();
 
+/**
+* @swagger
+* /:
+*   get:
+*     description: Devuelve un mensaje de estado 'pong' para verificar si el servidor está activo.
+*     responses:
+*       200:
+*         description: El servidor está activo y responde correctamente.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 message:
+*                   type: string
+*                   example: 'pong'
+*/
 router.get('/', pingController.get.bind(pingController));
 
 export default router;
